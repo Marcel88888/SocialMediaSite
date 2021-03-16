4c5d8b030e559a7008a699458fb84291e91ffcee
Marcel88888 <marcel.kawski1@gmail.com>
2021-03-16 17:49:26 +0100

    urls in urls.py added and admin.py completed for groups app.

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class GroupMemberInline(admin.TabularInline):  # to edit members is a group
+    model = models.GroupMember
+
+
+admin.site.register(models.Group)
